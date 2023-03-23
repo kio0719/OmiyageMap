14238ebedcccda15b20605f07e765a36a4edae9a
ioioio0719 <kio0719@docomo.ne.jp>
2023-03-23 13:56:28 +0900

    [add]deviseによるユーザー認証機能を追加

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,45 @@
+class UsersController < ApplicationController
+  before_action :set_user
+
+  def profile
+  end
+
+  def profile_edit
+  end
+
+  def profile_update
+    if @user.update(user_params)
+      flash[:notice] = "プロフィールの情報が更新されました"
+      redirect_to users_profile_path
+    else
+      flash[:warning] = "プロフィールの更新に失敗しました"
+      render "profile_edit"
+    end
+  end
+
+  def account
+  end
+  
+  def account_edit
+  end
+
+  def account_update
+    if @user.update(user_params)
+      flash[:notice] = "アカウントの情報が更新されました。"
+      redirect_to users_account_path
+    else
+      flash[:warning] = "アカウントの更新に失敗しました"
+      render "account_edit"
+    end
+  end
+
+  private
+
+  def set_user
+    @user = current_user
+  end
+
+  def user_params
+    params.require(:user).permit(:email, :name, :introduction)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  root :to => 'home#index' 
+  devise_for :users
+  get 'users/profile', to: 'users#profile'
+  get 'users/profile/edit', to: 'users#profile_edit'
+  patch 'users/profile/update', to: 'users#profile_update'
+  get 'users/account', to: 'users#account'
+  get 'users/account/edit', to: 'users#account_edit'
+  patch 'users/account/update', to: 'users#account_update'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
