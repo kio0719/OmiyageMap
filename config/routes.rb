008649fb93a5169ea76385ba583cee258978053e
ioioio0719 <kio0719@docomo.ne.jp>
2023-03-23 19:01:26 +0900

    [update]アカウント編集後のリダイレクト先をアカウント画面に変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   root :to => 'home#index' 
-  devise_for :users
+  devise_for :users, controllers: {
+    sessions: "users/sessions",
+    registrations: "users/registrations"
+  }
   get 'users/profile', to: 'users#profile'
   get 'users/profile/edit', to: 'users#profile_edit'
   patch 'users/profile/update', to: 'users#profile_update'
