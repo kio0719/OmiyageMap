bb567de9948cfba82dcf916554fc6a30593ab1ab
ioioio0719 <kio0719@docomo.ne.jp>
2023-04-01 15:26:57 +0900

    [remove]不要な記述、ファイルの削除

--- config/routes.rb
+++ config/routes.rb
@@ -7,8 +7,4 @@ Rails.application.routes.draw do
   get 'users/profile', to: 'users#profile'
   get 'users/profile/edit', to: 'users#profile_edit'
   patch 'users/profile/update', to: 'users#profile_update'
-  get 'users/account', to: 'users#account'
-  get 'users/account/edit', to: 'users#account_edit'
-  patch 'users/account/update', to: 'users#account_update'
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
