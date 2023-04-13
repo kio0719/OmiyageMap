466dd6bc909a9a77b81b799c39a8399780bd7888
ioioio0719 <kio0719@docomo.ne.jp>
2023-04-13 10:26:43 +0900

    [add]検索機能にリセットボタン、日付検索機能を追加。フリーワード検索を複数ワード検索できるよう変更

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,11 +2,29 @@ class HomeController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    if params[:q].blank?
-      params[:q] = { sorts: 'created_at desc'}
-    end
-      @q = Post.ransack(params[:q])
+      if params[:commit] == "検索" #フリーワード検索ボタン押した時
+        freeword_divise_search
+      elsif params[:commit] == "詳細検索" #詳細検索ボタン押した時
+        @q = Post.ransack(params[:q])
+      elsif params[:q].blank? #何もない時
+        params[:q] = { sorts: 'created_at desc'}
+        @q = Post.ransack(params[:q])
+      else #ソートボタン押した時
+        if params[:q][:name_or_address_or_caption_or_user_name_cont_any].present? #フリーワードあったら
+          freeword_divise_search
+        else #なかったら
+          @q = Post.ransack(params[:q])
+        end
+      end
       @results = @q.result.page(params[:page]).per(10)
-      @count = @results.count
+      @count = @q.result.count
+  end
+
+  private
+
+  def freeword_divise_search
+    key_words = params[:q][:name_or_address_or_caption_or_user_name_cont_any].split(/[\p{blank}\s]+/)
+    grouping_hash = key_words.reduce({}){|hash, word| hash.merge(word  => { name_or_address_or_caption_or_user_name_cont_any: word })}
+    @q = Post.ransack({ combinator: 'and', groupings: grouping_hash, sorts: params[:q][:sorts] })
   end
 end
---
+++ app/javascript/packs/reset.js
@@ -0,0 +1,6 @@
+$(function(){
+  $('#reset-button').on('click', function(){
+    $('input[type="search"]').val('');
+    $('input[type="date"]').val('');
+  });
+});
