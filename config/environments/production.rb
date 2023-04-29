935d4801a4f8dba412a7f889605d90fad86345b6
ioioio0719 <kio0719@docomo.ne.jp>
2023-04-29 15:41:06 +0900

    [add]CloudFrontを使用するように設定変更

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -31,7 +31,13 @@ Rails.application.configure do
   config.assets.compile = false
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  # config.asset_host = 'http://assets.example.com'
+  config.action_controller.asset_host = "https://#{Rails.application.credentials.dig(:aws, :cdn_host)}"
+
+  Rails.application.routes.default_url_options = {
+    host: Rails.application.credentials.dig(:aws, :cdn_host)
+    port: 443,
+    protocol: 'https'
+  }
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = 'X-Sendfile' # for Apache
---
+++ config/initializers/active_storage.rb
@@ -0,0 +1,1 @@
+Rails.application.config.active_storage.resolve_model_to_route = :cdn_proxy
