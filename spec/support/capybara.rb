d7737e4f303c52d17b06b0bfb4f565c919ad4e5e
ioioio0719 <kio0719@docomo.ne.jp>
2023-05-05 08:49:34 +0900

    [update]空白、インデント等view調整

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -3,7 +3,6 @@ require 'selenium-webdriver'
 
 Capybara.register_driver :selenium_chrome_headless do |app|
   options = ::Selenium::WebDriver::Chrome::Options.new
-
   options.add_argument('--headless')
   options.add_argument('--no-sandbox')
   options.add_argument('--disable-dev-shm-usage')
